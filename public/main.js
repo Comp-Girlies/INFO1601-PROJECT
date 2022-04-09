e1a7ae0c97832491eeddd04ada295ea081653fd2
Jus-Jar <jaredheeralal04@gmail.com>
2022-04-09 19:51:00 -0400

    search format

--- public/main.js
+++ public/main.js
@@ -18,7 +18,7 @@ async function getTopAnime(location){
       
       if( (mql.matches) === true){
           // to print 2 tiles per row.
-          printTopAnime2(result,location);// 3. Do something with the data
+          printTopAnime2(result,location);// 3. Do something with the data 
       }
 
       else{
@@ -43,7 +43,6 @@ async function getTopAnime(location){
     i=0;
     while(i<AnimeData.data.length){
 
-        // if (i ===AnimeData.data.length-1){
             let sym_rating = printRating(AnimeData.data[i].score);
             console.log(sym_rating);
             //build html string
@@ -140,7 +139,6 @@ function printTopAnime2(AnimeData,location){
     i=0;
     while(i<AnimeData.data.length){
 
-        // if (i ===AnimeData.data.length-1){
             let sym_rating = printRating(AnimeData.data[i].score);
             console.log(sym_rating);
             //build html string
@@ -156,8 +154,6 @@ function printTopAnime2(AnimeData,location){
                 </div>`
 
                 result.innerHTML = html;//add html string to DOM
-
-                
 
                 i = i + 1;
 
@@ -188,45 +184,181 @@ function printTopAnime2(AnimeData,location){
     }
 }
 
-//getData2(server,'#image');
-//getData2(server3,'#image2');
-//getData3("#top");
 
 async function getTrendingAnime(location){
     try{
       let response = await fetch(`https://api.jikan.moe/v4/seasons/now`);//1. Send http request and get response
       let result = await response.json();//2. Get data from response;
       console.log(result);
-      printTrendingAnime(result,location);// 3. Do something with the data
+      let mql = window.matchMedia('(max-width: 450px)');
+      if( (mql.matches) === true){
+        // to print 2 tiles per row.
+        printTrendingAnime2(result,location);// 3. Do something with the data 
+    }
+
+    else{
+        // 4 tiles per row
+        printTrendingAnime(result,location);// 3. Do something with the data
+    }
+      
     
    }catch(e){
        console.log(e);//catch and log any errors
    }
  }
 
+
 function printTrendingAnime(AnimeData,location){
     let result = document.querySelector(location);
     console.log(result);
     //add html code inside of result
     let html = '';// create html string
-    for(i=0;i<AnimeData.data.length;i = i+1){
+
+    i=0;
+    while(i<AnimeData.data.length){
 
             let sym_rating = printRating(AnimeData.data[i].score);
             console.log(sym_rating);
             //build html string
             html += `
+            <center>
             <div class="row">
-                <div class=" col s6 m4 l3 offset-s0 m0 l0">
+                <div class="col s6 l3">
                     <div class="section"> 
-                        <img  src="${AnimeData.data[i].images.jpg.image_url}" style="display:inline-block;"/>
-                        <h5>#${i+1}  ${AnimeData.data[i].title}</h5>
-                        <h5>Rating: ${AnimeData.data[i].score}/10  ${sym_rating}</h5>
+                            <img  src="${AnimeData.data[i].images.jpg.image_url}" style="display:inline-block;"/>
+                            <h5>#${i+1}  ${AnimeData.data[i].title}</h5>
+                            <h5>Rating: ${AnimeData.data[i].score}/10  ${sym_rating}</h5>
                     </div>
-            </div>`
+                </div>`
 
                 result.innerHTML = html;//add html string to DOM
+
+                i = i + 1;
+
+                if( i >= (AnimeData.data.length -1)){
+                    html += `</div>
+                    </center>`
+
+                    result.innerHTML = html;//add html string to DOM
+                }
+                else{
+                    html += `<div class="col s6 l3">
+                    <div class="section"> 
+                            <img  src="${AnimeData.data[i].images.jpg.image_url}" style="display:inline-block;"/>
+                            <h5>#${i+1}  ${AnimeData.data[i].title}</h5>
+                            <h5>Rating: ${AnimeData.data[i].score}/10  ${sym_rating}</h5>
+                    </div>
+                </div>`
+
+                result.innerHTML = html;//add html string to DOM
+                    
+                }
+
+                i = i + 1;
+
+                if( i >= (AnimeData.data.length -1)){
+                    html += `</div>
+                    </center>`
+
+                    result.innerHTML = html;//add html string to DOM
+                }
+                else{
+                    html += `<div class="col s6 l3">
+                    <div class="section"> 
+                            <img  src="${AnimeData.data[i].images.jpg.image_url}" style="display:inline-block;"/>
+                            <h5>#${i+1}  ${AnimeData.data[i].title}</h5>
+                            <h5>Rating: ${AnimeData.data[i].score}/10  ${sym_rating}</h5>
+                    </div>
+                </div>`
+
+                result.innerHTML = html;//add html string to DOM
+                    
+                }
+
+                i = i + 1;
+
+                if( i >= (AnimeData.data.length -1)){
+                    html += `</div>
+                    </center>`
+
+                    result.innerHTML = html;//add html string to DOM
+                }
+                else{
+                    html += `<div class="col s6 l3">
+                    <div class="section"> 
+                            <img  src="${AnimeData.data[i].images.jpg.image_url}" style="display:inline-block;"/>
+                            <h5>#${i+1}  ${AnimeData.data[i].title}</h5>
+                            <h5>Rating: ${AnimeData.data[i].score}/10  ${sym_rating}</h5>
+                    </div>
+                </div>
+                </div>
+                </center>
+                `
+
+                result.innerHTML = html;//add html string to DOM
+                    
+                }
+
+                i = i + 1;
+
     }
 }
+
+
+function printTrendingAnime2(AnimeData,location){
+    let result = document.querySelector(location);
+    console.log(result);
+    //add html code inside of result
+    let html = '';// create html string
+
+    i=0;
+    while(i<AnimeData.data.length){
+
+            let sym_rating = printRating(AnimeData.data[i].score);
+            console.log(sym_rating);
+            //build html string
+            html += `
+            <center>
+            <div class="row">
+                <div class="col s6 l3">
+                    <div class="section"> 
+                            <img  src="${AnimeData.data[i].images.jpg.image_url}" style="display:inline-block;"/>
+                            <h5>#${i+1} ${AnimeData.data[i].title}</h5>
+                            <h5>Rating: ${AnimeData.data[i].score}/10  ${sym_rating}</h5>
+                    </div>
+                </div>`
+
+                result.innerHTML = html;//add html string to DOM
+
+                i = i + 1;
+
+                if( i >= (AnimeData.data.length -1)){
+                    html += `</div>
+                    </center>`
+
+                    result.innerHTML = html;//add html string to DOM
+                }
+                else{
+                    html += `<div class="col s6 l3">
+                    <div class="section"> 
+                            <img  src="${AnimeData.data[i].images.jpg.image_url}" style="display:inline-block;"/>
+                            <h5>#${i+1}  ${AnimeData.data[i].title}</h5>
+                            <h5>Rating: ${AnimeData.data[i].score}/10  ${sym_rating}</h5>
+                    </div>
+                </div>
+                </div>
+                </center>
+                `
+
+                result.innerHTML = html;//add html string to DOM
+                    
+                }
+
+                i = i + 1;
+
+    }
+}
+
 
 function printRating(rating){
 
