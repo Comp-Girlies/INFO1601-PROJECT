26bdf8639c205c905eea56f18c916a2c88b89922
Jus-Jar <jaredheeralal04@gmail.com>
2022-04-09 10:06:27 -0400

    added trending anime

--- public/main.js
+++ public/main.js
@@ -226,28 +226,72 @@ async function getTopAnime(location){
 //getData2(server3,'#image2');
 //getData3("#top");
 
+async function getTrendingAnime(location){
+    try{
+      let response = await fetch(`https://api.jikan.moe/v4/seasons/now`);//1. Send http request and get response
+      let result = await response.json();//2. Get data from response;
+      console.log(result);
+      printTrendingAnime(result,location);// 3. Do something with the data
+    
+   }catch(e){
+       console.log(e);//catch and log any errors
+   }
+ }
+
+function printTrendingAnime(AnimeData,location){
+    let result = document.querySelector(location);
+    console.log(result);
+    //add html code inside of result
+    let html = '';// create html string
+    for(i=0;i<AnimeData.data.length;i = i+1){
+
+            let sym_rating = printRating(AnimeData.data[i].score);
+            console.log(sym_rating);
+            //build html string
+            html += `
+            <div class="row">
+                <div class="col s4 m3">
+                    <div class="section"> 
+                        <img  src="${AnimeData.data[i].images.jpg.image_url}" style="display:inline-block;"/>
+                    </div>
+                        <h5>#${i+1}  ${AnimeData.data[i].title}</h5>
+                        <h5>Rating: ${AnimeData.data[i].score}/10  ${sym_rating}</h5>
+                    </div>
+            </div>`
+
+                result.innerHTML = html;//add html string to DOM
+    }
+}
 
 function printRating(rating){
 
     let temp = rating;
     let sym_rating = "";
+    let count = 0;
 
     while (temp > 0){
 
         if (temp >= 2){
             sym_rating = sym_rating + "★";
             temp = temp -2;
+            count++;
         }
         else if (temp > 0 && temp < 2){
             sym_rating = sym_rating + "☆"; // insert no star symbol.
             temp = 0;
+            count++;
             
         }
 
     }
+    while(count < 5){
+        sym_rating = sym_rating + "☆";
+        count++;
+    }
+       
     return sym_rating;
 }
 
 
 getTopAnime('#top_anime');
-
+getTrendingAnime('#trending_anime');
