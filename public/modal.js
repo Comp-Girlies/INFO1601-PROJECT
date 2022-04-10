1053b64e5626316771a36397a2fe5887fac3806c
Jus-Jar <jaredheeralal04@gmail.com>
2022-04-10 15:49:33 -0400

    fixed modal layout

--- public/modal.js
+++ public/modal.js
@@ -1,10 +1,14 @@
 //This js file is to manage modal functionality
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.tabs');
     var instances = M.Modal.init(elems, options);
   });
+
+ 
 
 //    var instance = M.Modal.getInstance(elem);
 
@@ -76,6 +80,43 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 ///////////////////////////////////////////////////////////////////
+  // async function showDetails(AnimeData){
+  //   const modalhtml = document.querySelector('#modal1');
+  //   const modal = M.Modal.init(modalhtml);// get a modal object from materialize
+  //   console.log(AnimeData);
+   
+  //   modalhtml.innerHTML = `
+  //           <div id="modaltest" class="modal-content">
+  //             <div class="row">
+  //               <div class="col s4">
+    
+  //                 <img src="${AnimeData.data.images.jpg.image_url}" style="display:inline-block;"/>
+  //                 <h5>${AnimeData.data.title}</h5>
+                  
+  //               </div>
+  //               <div class="col s8">
+                
+  //                 <iframe width="400" height="200" src="${AnimeData.data.trailer.embed_url}" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  //               </div>
+  //             </div>
+  //             <h5>Synopsis:</h5>
+  //             <p>${AnimeData.data.synopsis}</p>
+    
+  //             <br><br><br>
+  //             <h5>Genres: ${AnimeData.data.genres[1]},${AnimeData.data.genres[2]}</h5>
+  //             <h5>Rating: ${AnimeData.data.score} / 10</h5>
+  //             <h5>Number of Episodes: ${AnimeData.data.epsiodes}</h5>
+  //           </div>
+  //           <div class="modal-footer">
+  //               <a href="#!" class="modal-close waves-effect waves-green btn-flat">Close</a>
+  //           </div>
+  //      `
+  
+  //   modal.open();//open the modal
+  
+  //  }
+
+  
   async function showDetails(AnimeData){
     const modalhtml = document.querySelector('#modal1');
     const modal = M.Modal.init(modalhtml);// get a modal object from materialize
@@ -83,33 +124,57 @@ document.addEventListener('DOMContentLoaded', function() {
    
     modalhtml.innerHTML = `
             <div id="modaltest" class="modal-content">
-              <div class="row">
-                <div class="col s4">
-    
-                  <img src="${AnimeData.data.images.jpg.image_url}" style="display:inline-block;"/>
-                  <h5>${AnimeData.data.title}</h5>
-                  
-                </div>
-                <div class="col s8">
-                
-                  <iframe width="400" height="200" src="${AnimeData.data.trailer.embed_url}" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                </div>
+              <div class="tab2">
+              <button class="tablinks" onclick="openInfo(event, 'anime_overview')">Anime Overview</button>
+              <button class="tablinks" onclick="openInfo(event, 'synopsis')">Synopsis</button>
+              <button class="tablinks" onclick="openInfo(event, 'other_info')">Other Info</button>
               </div>
-              <h5>Synopsis:</h5>
-              <p>${AnimeData.data.synopsis}</p>
-    
-              <br><br><br>
-              <h5>Genres: ${AnimeData.data.genres[1]},${AnimeData.data.genres[2]}</h5>
-              <h5>Rating: ${AnimeData.data.score} / 10</h5>
-              <h5>Number of Episodes: ${AnimeData.data.epsiodes}</h5>
+          
+              <div id="anime_overview" class="tabcontent">
+                <div class="row">
+                  <div class="col s4">
+                    <img src="${AnimeData.data.images.jpg.image_url}" style="display:inline-block;"/>
+                    <h5>${AnimeData.data.title}</h5>     
+                  </div>
+                  <div class="col s8">   
+                      <iframe width="400" height="200" src="${AnimeData.data.trailer.embed_url}" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                  </div>
+               </div>
+
+              </div>
+          
+              <div id="synopsis" class="tabcontent">
+                <h5>Synopsis:</h5>
+                <p>${AnimeData.data.synopsis}</p>
+
+              </div>
+          
+              <div id="other_info" class="tabcontent">
+              <h3>Tokyo</h3>
+              <p>Tokyo is the capital of Japan.</p>
             </div>
+              </div>
             <div class="modal-footer">
                 <a href="#!" class="modal-close waves-effect waves-green btn-flat">Close</a>
             </div>
+
        `
   
     modal.open();//open the modal
   
    }
 
-  
+   //
+   function openInfo(evt, view) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(view).style.display = "block";
+    evt.currentTarget.className += " active";
+    }
