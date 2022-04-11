b94b51fea56b71822ad2bd27dfebda0f1ba6f414
Jus-Jar <jaredheeralal04@gmail.com>
2022-04-11 10:51:28 -0400

    grid layout fix

--- public/manga_main.js
+++ public/manga_main.js
@@ -4,19 +4,19 @@ async function getTopManga(location){
       let response = await fetch(`https://api.jikan.moe/v4/top/manga`);//1. Send http request and get response
       let result = await response.json();//2. Get data from response;
       console.log(result);
-
-      let mql = window.matchMedia('(max-width: 450px)');
-      console.log(mql.matches);
+      printTopManga(result,location); 
+    //   let mql = window.matchMedia('(max-width: 450px)');
+    //   console.log(mql.matches);
       
-      if( (mql.matches) === true){
-          // to print 2 tiles per row.
-          printTopManga2(result,location);// 3. Do something with the data 
-      }
+    //   if( (mql.matches) === true){
+    //       // to print 2 tiles per row.
+    //       printTopManga2(result,location);// 3. Do something with the data 
+    //   }
 
-      else{
-          // 4 tiles per row
-        printTopManga(result,location);// 3. Do something with the data
-      }
+    //   else{
+    //       // 4 tiles per row
+    //     printTopManga(result,location);// 3. Do something with the data
+    //   }
 
       
     
@@ -31,185 +31,50 @@ async function getTopManga(location){
     console.log(result);
     //add html code inside of result
     let html = '';// create html string
-
-    i=0;
-    while(i<MangaData.data.length){
+    for(let i = 0; i < MangaData.data.length-1; i = i+4){
 
             let sym_rating = printRating(MangaData.data[i].score);
             console.log(sym_rating);
             //build html string
             html += `
-            <center>
-            <div class="row">
-                <div class="col s6 l3">
-                    <div class="section"> 
-                            <img onclick = "getAnimeInfo(${MangaData.data[i].mal_id})" src="${MangaData.data[i].images.jpg.image_url}" style="display:inline-block;"/>
-                            <h5 >#${MangaData.data[i].rank}  ${MangaData.data[i].title}</h5>
-                            <h5>Rating: ${MangaData.data[i].score}/10  ${sym_rating}</h5>
-                    </div>
-                </div>`
-
-                result.innerHTML = html;//add html string to DOM
-
-                i = i + 1;
-
-                if( i >= (MangaData.data.length -1)){
-                    html += `</div>
-                    </center>`
-
-                    result.innerHTML = html;//add html string to DOM
-                }
-                else{
-                    html += `<div class="col s6 l3">
-                    <div class="section"> 
-                            <img  src="${MangaData.data[i].images.jpg.image_url}" style="display:inline-block;"/>
-                            <h5>#${MangaData.data[i].rank}  ${MangaData.data[i].title}</h5>
-                            <h5>Rating: ${MangaData.data[i].score}/10  ${sym_rating}</h5>
-                    </div>
-                </div>`
-
-                result.innerHTML = html;//add html string to DOM
-                    
-                }
-
-                i = i + 1;
-
-                if( i >= (MangaData.data.length -1)){
-                    html += `</div>
-                    </center>`
-
-                    result.innerHTML = html;//add html string to DOM
-                }
-                else{
-                    html += `<div class="col s6 l3">
-                    <div class="section"> 
-                            <img  src="${MangaData.data[i].images.jpg.image_url}" style="display:inline-block;"/>
-                            <h5>#${MangaData.data[i].rank}  ${MangaData.data[i].title}</h5>
-                            <h5>Rating: ${MangaData.data[i].score}/10  ${sym_rating}</h5>
-                    </div>
-                </div>`
-
-                result.innerHTML = html;//add html string to DOM
-                    
-                }
-
-                i = i + 1;
-
-                if( i >= (MangaData.data.length -1)){
-                    html += `</div>
-                    </center>`
-
-                    result.innerHTML = html;//add html string to DOM
-                }
-                else{
-                    html += `<div class="col s6 l3">
-                    <div class="section"> 
-                            <img  src="${MangaData.data[i].images.jpg.image_url}" style="display:inline-block;"/>
-                            <h5>#${MangaData.data[i].rank}  ${MangaData.data[i].title}</h5>
-                            <h5>Rating: ${MangaData.data[i].score}/10  ${sym_rating}</h5>
-                    </div>
+            <div class="flex">
+                <div class="sections"> 
+                        <img onclick = "getAnimeInfo(${MangaData.data[i].mal_id})" src="${MangaData.data[i].images.jpg.image_url}" style="display:inline-block;"/>
+                        <h5 >#${MangaData.data[i].rank}  ${MangaData.data[i].title}</h5>
+                        <h5>Rating: ${MangaData.data[i].score}/10  ${sym_rating}</h5>
                 </div>
+                <div class="sections"> 
+                        <img onclick = "getAnimeInfo(${MangaData.data[i+1].mal_id})" src="${MangaData.data[i+1].images.jpg.image_url}" style="display:inline-block;"/>
+                        <h5 >#${MangaData.data[i+1].rank}  ${MangaData.data[i+1].title}</h5>
+                        <h5>Rating: ${MangaData.data[i+1].score}/10  ${sym_rating}</h5>
                 </div>
-                </center>
+                <div class="sections"> 
+                        <img onclick = "getAnimeInfo(${MangaData.data[i+2].mal_id})" src="${MangaData.data[i+2].images.jpg.image_url}" style="display:inline-block;"/>
+                        <h5 >#${MangaData.data[i+2].rank}  ${MangaData.data[i+2].title}</h5>
+                        <h5>Rating: ${MangaData.data[i+2].score}/10  ${sym_rating}</h5>
+                </div>
+                <div class="sections"> 
+                        <img onclick = "getAnimeInfo(${MangaData.data[i+3].mal_id})" src="${MangaData.data[i+3].images.jpg.image_url}" style="display:inline-block;"/>
+                        <h5 >#${MangaData.data[i+3].rank}  ${MangaData.data[i+3].title}</h5>
+                        <h5>Rating: ${MangaData.data[i+3].score}/10  ${sym_rating}</h5>
+                </div>
+                
+            </div>    
                 `
-
-                result.innerHTML = html;//add html string to DOM
-                    
-                }
-
-                i = i + 1;
-
     }
+    result.innerHTML = html;//add html string to DOM
 }
 
-function printTopManga2(MangaData,location){
-    let result = document.querySelector(location);
-    console.log(result);
-    //add html code inside of result
-    let html = '';// create html string
-
-    i=0;
-    while(i<MangaData.data.length){
-
-            let sym_rating = printRating(MangaData.data[i].score);
-            console.log(sym_rating);
-            //build html string
-            html += `
-            <center>
-            <div class="row">
-                <div class="col s6 l3">
-                    <div class="section"> 
-                            <img  src="${MangaData.data[i].images.jpg.image_url}" style="display:inline-block;"/>
-                            <h5>#${MangaData.data[i].rank}  ${MangaData.data[i].title}</h5>
-                            <h5>Rating: ${MangaData.data[i].score}/10  ${sym_rating}</h5>
-                    </div>
-                </div>`
-
-                result.innerHTML = html;//add html string to DOM
-
-                i = i + 1;
-
-                if( i >= (MangaData.data.length -1)){
-                    html += `</div>
-                    </center>`
-
-                    result.innerHTML = html;//add html string to DOM
-                }
-                else{
-                    html += `<div class="col s6 l3">
-                    <div class="section"> 
-                            <img  src="${MangaData.data[i].images.jpg.image_url}" style="display:inline-block;"/>
-                            <h5>#${MangaData.data[i].rank}  ${MangaData.data[i].title}</h5>
-                            <h5>Rating: ${MangaData.data[i].score}/10  ${sym_rating}</h5>
-                    </div>
-                </div>
-                </div>
-                </center>
-                `
-
-                result.innerHTML = html;//add html string to DOM
-                    
-                }
-
-                i = i + 1;
-
-    }
-}
-
-
-// async function getTrendingAnime(location){
-//     try{
-//       let response = await fetch(`https://api.jikan.moe/v4/seasons/now`);//1. Send http request and get response
-//       let result = await response.json();//2. Get data from response;
-//       console.log(result);
-//       let mql = window.matchMedia('(max-width: 450px)');
-//       if( (mql.matches) === true){
-//         // to print 2 tiles per row.
-//         printTrendingAnime2(result,location);// 3. Do something with the data 
-//     }
-
-//     else{
-//         // 4 tiles per row
-//         printTrendingAnime(result,location);// 3. Do something with the data
-//     }
-      
-    
-//    }catch(e){
-//        console.log(e);//catch and log any errors
-//    }
-//  }
-
-
-// function printTrendingAnime(AnimeData,location){
+// function printTopManga2(MangaData,location){
 //     let result = document.querySelector(location);
 //     console.log(result);
 //     //add html code inside of result
 //     let html = '';// create html string
 
 //     i=0;
-//     while(i<AnimeData.data.length){
+//     while(i<MangaData.data.length){
 
-//             let sym_rating = printRating(AnimeData.data[i].score);
+//             let sym_rating = printRating(MangaData.data[i].score);
 //             console.log(sym_rating);
 //             //build html string
 //             html += `
@@ -217,9 +82,9 @@ function printTopManga2(MangaData,location){
 //             <div class="row">
 //                 <div class="col s6 l3">
 //                     <div class="section"> 
-//                             <img  src="${AnimeData.data[i].images.jpg.image_url}" style="display:inline-block;"/>
-//                             <h5>#${i+1}  ${AnimeData.data[i].title}</h5>
-//                             <h5>Rating: ${AnimeData.data[i].score}/10  ${sym_rating}</h5>
+//                             <img  src="${MangaData.data[i].images.jpg.image_url}" style="display:inline-block;"/>
+//                             <h5>#${MangaData.data[i].rank}  ${MangaData.data[i].title}</h5>
+//                             <h5>Rating: ${MangaData.data[i].score}/10  ${sym_rating}</h5>
 //                     </div>
 //                 </div>`
 
@@ -227,7 +92,7 @@ function printTopManga2(MangaData,location){
 
 //                 i = i + 1;
 
-//                 if( i >= (AnimeData.data.length -1)){
+//                 if( i >= (MangaData.data.length -1)){
 //                     html += `</div>
 //                     </center>`
 
@@ -236,51 +101,9 @@ function printTopManga2(MangaData,location){
 //                 else{
 //                     html += `<div class="col s6 l3">
 //                     <div class="section"> 
-//                             <img  src="${AnimeData.data[i].images.jpg.image_url}" style="display:inline-block;"/>
-//                             <h5>#${i+1}  ${AnimeData.data[i].title}</h5>
-//                             <h5>Rating: ${AnimeData.data[i].score}/10  ${sym_rating}</h5>
-//                     </div>
-//                 </div>`
-
-//                 result.innerHTML = html;//add html string to DOM
-                    
-//                 }
-
-//                 i = i + 1;
-
-//                 if( i >= (AnimeData.data.length -1)){
-//                     html += `</div>
-//                     </center>`
-
-//                     result.innerHTML = html;//add html string to DOM
-//                 }
-//                 else{
-//                     html += `<div class="col s6 l3">
-//                     <div class="section"> 
-//                             <img  src="${AnimeData.data[i].images.jpg.image_url}" style="display:inline-block;"/>
-//                             <h5>#${i+1}  ${AnimeData.data[i].title}</h5>
-//                             <h5>Rating: ${AnimeData.data[i].score}/10  ${sym_rating}</h5>
-//                     </div>
-//                 </div>`
-
-//                 result.innerHTML = html;//add html string to DOM
-                    
-//                 }
-
-//                 i = i + 1;
-
-//                 if( i >= (AnimeData.data.length -1)){
-//                     html += `</div>
-//                     </center>`
-
-//                     result.innerHTML = html;//add html string to DOM
-//                 }
-//                 else{
-//                     html += `<div class="col s6 l3">
-//                     <div class="section"> 
-//                             <img  src="${AnimeData.data[i].images.jpg.image_url}" style="display:inline-block;"/>
-//                             <h5>#${i+1}  ${AnimeData.data[i].title}</h5>
-//                             <h5>Rating: ${AnimeData.data[i].score}/10  ${sym_rating}</h5>
+//                             <img  src="${MangaData.data[i].images.jpg.image_url}" style="display:inline-block;"/>
+//                             <h5>#${MangaData.data[i].rank}  ${MangaData.data[i].title}</h5>
+//                             <h5>Rating: ${MangaData.data[i].score}/10  ${sym_rating}</h5>
 //                     </div>
 //                 </div>
 //                 </div>
@@ -296,60 +119,6 @@ function printTopManga2(MangaData,location){
 //     }
 // }
 
-
-// function printTrendingAnime2(AnimeData,location){
-//     let result = document.querySelector(location);
-//     console.log(result);
-//     //add html code inside of result
-//     let html = '';// create html string
-
-//     i=0;
-//     while(i<AnimeData.data.length){
-
-//             let sym_rating = printRating(AnimeData.data[i].score);
-//             console.log(sym_rating);
-//             //build html string
-//             html += `
-//             <center>
-//             <div class="row">
-//                 <div class="col s6 l3">
-//                     <div class="section"> 
-//                             <img  src="${AnimeData.data[i].images.jpg.image_url}" style="display:inline-block;"/>
-//                             <h5>#${i+1} ${AnimeData.data[i].title}</h5>
-//                             <h5>Rating: ${AnimeData.data[i].score}/10  ${sym_rating}</h5>
-//                     </div>
-//                 </div>`
-
-//                 result.innerHTML = html;//add html string to DOM
-
-//                 i = i + 1;
-
-//                 if( i >= (AnimeData.data.length -1)){
-//                     html += `</div>
-//                     </center>`
-
-//                     result.innerHTML = html;//add html string to DOM
-//                 }
-//                 else{
-//                     html += `<div class="col s6 l3">
-//                     <div class="section"> 
-//                             <img  src="${AnimeData.data[i].images.jpg.image_url}" style="display:inline-block;"/>
-//                             <h5>#${i+1}  ${AnimeData.data[i].title}</h5>
-//                             <h5>Rating: ${AnimeData.data[i].score}/10  ${sym_rating}</h5>
-//                     </div>
-//                 </div>
-//                 </div>
-//                 </center>
-//                 `
-
-//                 result.innerHTML = html;//add html string to DOM
-                    
-//                 }
-
-//                 i = i + 1;
-
-//     }
-// }
 
 
 function printRating(rating){
